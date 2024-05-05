71b6d6b5ba2d0219ff917e4271419db0f9b47ad4
zoe2276 <zoe22761@gmail.com>
2024-05-05 18:57:56 -0500

    implemention of iterative profile card creation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import * as React from "react";
 import './App.css';
 import { ProfileCard } from './components/ProfileCard';
 
 function App() {
   const testUser = require('./testUser.json')
+  
+  // Iterate over the data json file passed in and add ProfileCards for all users
+  let userCards = [];
+  for (let i = 0; i < testUser.length; i++) {
+    userCards[i] = <ProfileCard data={testUser[i]} />
+  }
+
   return (
     <div className="App">
-      <ProfileCard data={testUser[0]} />
-      <ProfileCard data={testUser[1]} />
+      {userCards ? userCards : "error with da users"}
     </div>
   );
 }
